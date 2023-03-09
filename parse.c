f25be097f030b6171a702d308b01be160f9021f7
MasatoNozaki <noza4ki@gmail.com>
2023-03-09 23:00:43 +0900

    文脈自由文法に対応した関数の実装

--- parse.c
+++ parse.c
@@ -7,6 +7,12 @@ bool consume(char op) {
   return true;
 }
 
+void expect(char op) {
+  if (token->kind != TK_SYMBOL || token->str[0] != op)
+    error("'%c'ではありません", op);
+  token = token->next;
+}
+
 int expect_number() {
   if (token->kind != TK_NUM)
     error_at(token->str, "数ではありません");
@@ -28,4 +34,42 @@ Node *new_node_num(int val) {
   node->kind = ND_NUM;
   node->val = val;
   return node;
+}
+
+Node *expr() {
+  Node *node = mul();
+
+  for (;;) {
+    if (consume('+'))
+      node = new_node(ND_ADD, node, mul());
+    else if (consume('-'))
+      node = new_node(ND_SUB, node, mul());
+    else
+      return node;
+  }
+}
+
+Node *mul() {
+  Node *node = primary();
+
+  for (;;) {
+    if (consume('*'))
+      node = new_node(ND_MUL, node, primary());
+    else if (consume('/'))
+      node = new_node(ND_DIV, node, primary());
+    else
+      return node;
+  }
+}
+
+Node *primary() {
+  // 次のトークンが"("なら、"(" expr ")"のはず
+  if (consume('(')) {
+    Node *node = expr();
+    expect(')');
+    return node;
+  }
+
+  // そうでなければ数値のはず
+  return new_node_num(expect_number());
 }
--- parse.h
+++ parse.h
@@ -27,6 +27,11 @@ struct Node {
 // 真を返す。それ以外の場合には偽を返す。
 bool consume(char op);
 
+// 次のトークンが期待している記号のときには、トークンを1つ読み進める。
+// それ以外の場合にはエラーを報告する。
+// consumeとの差別化点は、返り値がないこと。(expr)の解析の時、(exprまである場合は、)をチェックする場合わざわざif文でチェックする必要はない。
+void expect(char op);
+
 // 次のトークンが数値の場合、トークンを1つ読み進めてその数値を返す。
 // それ以外の場合にはエラーを報告する。
 int expect_number();
@@ -34,4 +39,8 @@ int expect_number();
 // 数字ノードは葉、それ以外は内部ノードであるため、分けている
 Node *new_node(NodeKind kind, Node *lhs, Node *rhs);
 Node *new_node_num(int val);
+
+Node *expr();
+Node *mul();
+Node *primary();
 #endif // __PARSE_H__

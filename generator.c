3e83626de1bfd4bae57c1a286521ae67e09be79d
MasatoNozaki <noza4ki@gmail.com>
2023-03-09 23:39:18 +0900

    mainの実装 コードの共通部分はmainにて記述

--- generator.c
+++ generator.c
@@ -1,11 +1,6 @@
 #include "generator.h"
 
 void gen() {
-	// アセンブリの前半部分を出力
-	printf(".intel_syntax noprefix\n");
-	printf(".globl main\n");
-	printf("main:\n");
-
 	// 式の最初は数でなければならないので、それをチェックして
 	// 最初のmov命令を出力
 	printf("  mov rax, %d\n", expect_number());
@@ -22,37 +17,35 @@ void gen() {
 
 		printf("error");
 	}
-
-	printf("	ret\n");
 }
 
 void gen_from_abstructTree(Node *node) {
-  if (node->kind == ND_NUM) {
-    printf("  push %d\n", node->val);
-    return;
-  }
+	if (node->kind == ND_NUM) {
+		printf("  push %d\n", node->val);
+		return;
+	}
 
-  gen(node->lhs);
-  gen(node->rhs);
+	gen(node->lhs);
+	gen(node->rhs);
 
-  printf("  pop rdi\n");
-  printf("  pop rax\n");
+	printf("  pop rdi\n");
+	printf("  pop rax\n");
 
-  switch (node->kind) {
-  case ND_ADD:
-    printf("  add rax, rdi\n");
-    break;
-  case ND_SUB:
-    printf("  sub rax, rdi\n");
-    break;
-  case ND_MUL:
-    printf("  imul rax, rdi\n");
-    break;
-  case ND_DIV:
-    printf("  cqo\n");
-    printf("  idiv rdi\n");
-    break;
-  }
+	switch (node->kind) {
+	case ND_ADD:
+		printf("  add rax, rdi\n");
+		break;
+	case ND_SUB:
+		printf("  sub rax, rdi\n");
+		break;
+	case ND_MUL:
+		printf("  imul rax, rdi\n");
+		break;
+	case ND_DIV:
+		printf("  cqo\n");
+		printf("  idiv rdi\n");
+		break;
+	}
 
-  printf("  push rax\n");
+	printf("  push rax\n");
 }
--- main.c
+++ main.c
@@ -10,9 +10,20 @@ int main(int argc, char **argv) {
 	}
 
 	user_input = argv[1];
-
 	token = tokenize(user_input);
+	Node *node = expr();
 
-	gen();
+	// アセンブリの前半部分を出力
+	printf(".intel_syntax noprefix\n");
+	printf(".globl main\n");
+	printf("main:\n");
+
+	gen_from_abstructTree(node);
+
+	// スタックトップに式全体の値が残っているはずなので
+	// それをRAXにロードして関数からの返り値とする
+	printf("  pop rax\n");
+	printf("  ret\n");
+	
 	return 0;
 }

83e1165f754c7feb17067411841da939e6efef1b
MasatoNozaki <noza4ki@gmail.com>
2023-03-09 22:52:20 +0900

    ノードの型とノード生成関数を追加

--- parse.h
+++ parse.h
@@ -4,6 +4,25 @@
 #include <stdbool.h>
 #include "tokenize.h"
 
+// 抽象構文木のノードの種類
+typedef enum {
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_MUL, // *
+  ND_DIV, // /
+  ND_NUM, // 整数
+} NodeKind;
+
+typedef struct Node Node;
+
+// 抽象構文木のノードの型
+struct Node {
+  NodeKind kind; // ノードの型
+  Node *lhs;     // 左辺
+  Node *rhs;     // 右辺
+  int val;       // kindがND_NUMの場合のみ使う
+};
+
 // 次のトークンが期待している記号のときには、トークンを1つ読み進めて
 // 真を返す。それ以外の場合には偽を返す。
 bool consume(char op);
@@ -11,4 +30,8 @@ bool consume(char op);
 // 次のトークンが数値の場合、トークンを1つ読み進めてその数値を返す。
 // それ以外の場合にはエラーを報告する。
 int expect_number();
+
+// 数字ノードは葉、それ以外は内部ノードであるため、分けている
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs);
+Node *new_node_num(int val);
 #endif // __PARSE_H__
